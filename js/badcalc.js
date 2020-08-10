7129ce823720f355e9e658389fb19a8b17ce80c4
triemers <triemers12@gmail.com>
2020-08-10 13:17:20 -0400

    doesn't work

--- js/badcalc.js
+++ js/badcalc.js
@@ -25,3 +25,65 @@ const handleClick = (e) => {
 
 //event listener
 keys.addEventListener('click', handleClick);
+
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    if (!action) {
+      if (displayedNum === '0') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent //can add mathfloor here?
+      };
+    };
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    };
+    if (action=== 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide') {
+          key.classList.add('is-depressed')
+          calculator.dataset.previousKeyType = 'operator';
+    };
+    const previousKeyType = calculator.dataset.previousKeyType
+if (!action) {
+  if (displayedNum === '0' || previousKeyType === 'operator') {
+    display.textContent = keyContent
+  } else {
+    display.textContent = displayedNum + keyContent
+  }
+}
+if (action === 'calculate') {
+  const secondValue = displayedNum
+  // ...
+}
+if (
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  // ...
+  calculator.dataset.firstValue = displayedNum
+  calculator.dataset.operator = action
+}
+  }
+
+})
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    // ...
+    
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+  }
+})
