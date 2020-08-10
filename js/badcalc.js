852da1a40a27943884057970f6f5fdc4680e3e43
triemers <triemers12@gmail.com>
2020-08-10 15:22:12 -0400

    js - need to fix classes?

--- js/badcalc.js
+++ js/badcalc.js
@@ -1,89 +1,89 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
+const decimal = document.getElementsByClassName('decimal');
+const clear = document.getElementsByClassName('clear');
+// Selects a h1 element that holds the input and result
+const displayValElement = document.getElementsByClassName('calculator__display');
+// Selects the buttons with the 10 digits (0–9)
+const btnNumbers = document.getElementsByClassName('btn-number');
+// Selects the four operators (add/subtract/divide/multiply) and the equals operator
+const btnOperators = document.getElementsByClassName('key--operator');
 
-// Looks cleaner, does the same thing as before
-const operators = ['add', 'subtract', 'multiply', 'divide']
+var displayVal ="0";
+var pendingVal;
+var evalStringArray = [];
 
-const handleClick = (e) => {
-  //determine type of key
-  const key = e.target
-  const action = key.dataset.action
-  if (!action) {
-    console.log('number key!')
-  } else if (operators.includes(action)) {
-    console.log('operator key!')
-  } else if (action === 'decimal') {
-    console.log('decimal key!')
-  } else if (action === 'clear') {
-    console.log('clear key!')
-  } else if (action === 'calculate') {
-    console.log('equal key')
-  } else {
-  	console.log('wtf')
+// Updating the display field
+updateDisplayVal = (e) => {
+  var btnText = e.target.innerText;
+  if(displayVal === '0') { 
+     displayVal = ''; 
   }
+// Append the content of the button we clicked to our displayVal variable and display it
+  displayVal += btnText; 
+  displayValElement.innerText = displayVal;
+}
+
+performOperation = (e) => {
+  var operator = e.target.innerText;
+  switch (operator) {
+      case '+':
+          pendingVal = displayVal;
+          displayVal = '0';
+          displayValElement.innerText = displayVal;
+          evalStringArray.push(pendingVal);
+          evalStringArray.push('+');
+          break;
+      case '-':
+          pendingVal = displayVal;
+          displayVal = '0';
+          displayValElement.innerText = displayVal;
+          evalStringArray.push(pendingVal);
+          evalStringArray.push('-');
+          break;
+      case '&times;':
+          pendingVal = displayVal;
+          displayVal = '0';
+          displayValElement.innerText = displayVal;
+          evalStringArray.push(pendingVal);
+          evalStringArray.push('*');
+          break;
+      case '÷':
+          pendingVal = displayVal;
+          displayVal = '0';
+          displayValElement.innerText = displayVal;
+          evalStringArray.push(pendingVal);
+          evalStringArray.push('/');
+          break;
+      case '=':
+          evalStringArray.push(displayVal);
+          var evaluation = eval(evalStringArray.join(' '));
+          displayVal = evaluation + ''; 
+          displayValElement.innerText = displayVal;
+          evalStringArray = []; // clear the array
+          break;
+      default:
+          break;
+  }
+}
+//event listeners
+for (let i = 0; i < btnNumbers.length; i++) {
+  btnNumbers[i].addEventListener('click', updateDisplayVal) 
+};
+for (let i =0; i < btnOperators.length; i++) {
+  btnOperators[i].addEventListener('click', performOperation)
 };
 
-//event listener
-keys.addEventListener('click', handleClick);
-
-const display = document.querySelector('.calculator__display')
-
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    const key = e.target
-    const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
-    if (!action) {
-      if (displayedNum === '0') {
-        display.textContent = keyContent
-      } else {
-        display.textContent = displayedNum + keyContent //can add mathfloor here?
-      };
-    };
-    if (action === 'decimal') {
-      display.textContent = displayedNum + '.'
-    };
-    if (action=== 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide') {
-          key.classList.add('is-depressed')
-          calculator.dataset.previousKeyType = 'operator';
-    };
-    const previousKeyType = calculator.dataset.previousKeyType
-if (!action) {
-  if (displayedNum === '0' || previousKeyType === 'operator') {
-    display.textContent = keyContent
-  } else {
-    display.textContent = displayedNum + keyContent
-  }
+// On clicking the clear button, all values and the display are being reset 
+clear.onclick = () => {
+  displayVal = '0';
+  pendingVal = undefined;
+  evalStringArray = [];
+  displayValElement.innerHTML = displayVal;
 }
-if (action === 'calculate') {
-  const secondValue = displayedNum
-  // ...
-}
-if (
-  action === 'add' ||
-  action === 'subtract' ||
-  action === 'multiply' ||
-  action === 'divide'
-) {
-  // ...
-  calculator.dataset.firstValue = displayedNum
-  calculator.dataset.operator = action
-}
-  }
 
-})
-
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    const key = e.target
-    // ...
-    
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'))
+// Not allowing two decimal points in input
+decimal.onclick = () => { 
+  if(!displayVal.includes('.')) {
+      displayVal += '.';
   }
-})
+  displayValElement.innerText = displayVal;
+}
